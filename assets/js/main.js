a7d580e382e6d427d715451b8b5f3c6caeed5cbd
AshutoshTutwani <ashutosh.tutwani@highlands.in>
2020-10-06 13:46:06 +0530

    remove owl

--- assets/js/main.js
+++ assets/js/main.js
@@ -182,38 +182,6 @@
     });
   });
 
-  // Events carousel (uses the Owl Carousel library)
-  $(".events-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
-
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    autoplayTimeout: 6000,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 2
-      },
-      900: {
-        items: 3
-      }
-    }
-  });
-
-  // Initiate venobox lightbox
-  $(document).ready(function() {
-    $('.venobox').venobox();
-  });
-
   // Init AOS
   function aos_init() {
     AOS.init({
